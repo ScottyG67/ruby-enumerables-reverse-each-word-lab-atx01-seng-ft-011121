059bf2327cbe6a9118a041dddd54ce1e754be22e
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-28 20:29:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,22 +1,10 @@
 def reverse_each_word(sentence)
   require 'pry'
-  #new_sentence = String.new
-  #array = sentence.split(" ")
-  #binding.pry
-  #array.collect do |element|
-    #binding.pry
-    #new_sentence=new_sentence + element.reverse + " "
-  #end
-  #new_sentence.chop!
-  
-  #new_sentence = String.new
+
   new_array= []
   array = sentence.split(" ")
-  #binding.pry
   new_array = array.collect do |element|
-    #binding.pry
     element.reverse
   end
-  new_array*" "
-#binding.pry
+  new_array * " "
 end
